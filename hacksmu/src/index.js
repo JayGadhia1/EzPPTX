6b517b137fda425f657847b53ce5dfc312e01619
KFry101 <katiefrymire@gmail.com>
2024-10-05 19:02:54 -0500

    AI ADD

--- hacksmu/src/index.js
+++ hacksmu/src/index.js
@@ -3,7 +3,12 @@ import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 addOnUISdk.ready.then(() => {
     console.log("addOnUISdk is ready for use.");
 
-    const clickMeButton = document.getElementById("clickMe");
+    const clickMeButton = document.getElementById("Export");
+    clickMeButton.addEventListener("click", () => {
+        clickMeButton.innerHTML = "Clicked";
+    });
+
+    const clickMeButtonAI = document.getElementById("ExportAI");
     clickMeButton.addEventListener("click", () => {
         clickMeButton.innerHTML = "Clicked";
     });
